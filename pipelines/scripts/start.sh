4f24381e08c2298dae33308d96385b21a8bfbb6d
Dmitriy Yevtushenko <dmitriy.yevtushenko@gmail.com>
2018-03-03 17:08:36 +0200

    not working

--- pipelines/scripts/start.sh
+++ pipelines/scripts/start.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-if [ -z BD_SCHEME ]; then
+if [ -n BD_SCHEME ]; then
  cmd_opts="$cmd_opts --scheme=http"
 else
  cmd_opts="$cmd_opts --scheme=$BD_SCHEME"
 fi
 
 if [ -n PORT ]; then
+ cmd_opts="$cmd_opts --port=11111"
+else
  cmd_opts="$cmd_opts --port=$PORT"
 fi
 
 if [ -n HOST ]; then
+ cmd_opts="$cmd_opts --host=localhost"
+else
  cmd_opts="$cmd_opts --host=$HOST"
 fi
 
-if [ -z CONFIG ]; then
+if [ -n CONFIG ]; then
  cmd_opts="$cmd_opts --config=docker"
 else
  cmd_opts="$cmd_opts --config=$CONFIG"
 fi
 
-if [ -z CONFIG_FILE ]; then
+if [ -n CONFIG_FILE ]; then
  cmd_opts="$cmd_opts --config-file=./weaviate.conf.json"
 else
  cmd_opts="$cmd_opts --config-file=$CONFIG_FILE"
 fi
 
-exec /var/weaviate/weaviate $cmd_opts > /var/weaviate/first_run.log 2>&1
+echo $cmd_opts
+exec /var/weaviate/weaviate $cmd_opts
