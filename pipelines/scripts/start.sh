e1826e736eb329a269d078713d2d941fa45ec1ee
Dmitriy Yevtushenko <dmitriy.yevtushenko@gmail.com>
2018-03-05 14:43:30 +0200

    minor fixes of script

--- pipelines/scripts/start.sh
+++ pipelines/scripts/start.sh
@@ -1,51 +1,30 @@
 #!/bin/bash
 
-if [ -n $WEAVIATE_SCHEME ]; then
- cmd_opts="$cmd_opts --scheme=http"
-else
- cmd_opts="$cmd_opts --scheme=$WEAVIATE_SCHEME"
-fi
+# Defination of a env variables with default values
+WEAVIATE_SCHEME=${WEAVIATE_SCHEME:-http}
+WEAVIATE_PORT=${WEAVIATE_PORT:-80}
+WEAVIATE_HOST=${WEAVIATE_HOST:-0.0.0.0}
+WEAVIATE_CONFIG=${WEAVIATE_CONFIG:-cassandra}
+WEAVIATE_CONFIG_FILE=${WEAVIATE_CONFIG_FILE:-./weaviate.conf.json}
 
-if [ -n $WEAVIATE_PORT ]; then
- cmd_opts="$cmd_opts --port=80"
-else
- cmd_opts="$cmd_opts --port=$WEAVIATE_PORT"
-fi
-
-if [ -n $WEAVIATE_HOST ]; then
- cmd_opts="$cmd_opts --host=0.0.0.0"
-else
- cmd_opts="$cmd_opts --host=$WEAVIATE_HOST"
-fi
-
-if [ -n $WEAVIATE_CONFIG ]; then
- cmd_opts="$cmd_opts --config=cassandra"
-else
- cmd_opts="$cmd_opts --config=$WEAVIATE_CONFIG"
-fi
-
-if [ -n $WEAVIATE_CONFIG_FILE ]; then
- cmd_opts="$cmd_opts --config-file=./weaviate.conf.json"
-else
- cmd_opts="$cmd_opts --config-file=$WEAVIATE_CONFIG_FILE"
-fi
-
-
+# Loop for checking connection to cassandra DB
 if [ "$WEAVIATE_CONFIG" == "cassandra" ]; then
- counter=0
- until cqlsh --cqlversion=3.4.4 "$WEAVIATE_CASSANDRA_DB_HOST" -e exit; do
-   >&2 echo "Cassandra is unavailable - sleeping"
-   sleep 3
-   ((counter++))
-   if [ $counter -eq 10 ]; then
-     echo "cassandra is not available trying to start without cassandra"
-     break 
-   fi
- done
+  counter=1
+  until cqlsh --cqlversion=3.4.4 "$WEAVIATE_CASSANDRA_DB_HOST" -e exit; do
+    >&2 echo "Cassandra is unavailable - sleeping"
+    sleep 3
+    ((counter++))
+    if [ $counter -gt 10 ]; then
+      echo "cassandra is not available trying to start without cassandra"
+      break 
+    fi
+  done
 else
- echo "DB is not cassandra"
+  echo "DB is not cassandra"
 fi
 
 
 cd /var/weaviate/
-exec ./weaviate $cmd_opts
+
+# Starting main process of container
+exec ./weaviate --scheme=${WEAVIATE_SCHEME} --port=${WEAVIATE_PORT} --host=${WEAVIATE_HOST} --config=${WEAVIATE_CONFIG} --config-file=${WEAVIATE_CONFIG_FILE}
